01919488a63e9272fe5948ab386d853e2f715c9a
Alfred Gårdeskog <kaffeskogen@outlook.com>
2024-07-14 23:25:37 +0200

    more signals!

--- src/app/settings/ui/general-settings/general-settings.component.ts
+++ src/app/settings/ui/general-settings/general-settings.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, inject } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { GoogleDriveStorageProvider } from 'src/app/shared/data-access/google-drive-storage-provider';
 import { StorageService } from 'src/app/shared/data-access/storage.service';
 import { TransactionsService } from 'src/app/shared/data-access/transactions.service';
@@ -12,12 +12,12 @@ import { TransactionsService } from 'src/app/shared/data-access/transactions.ser
     
     <label class="flex flex-col mt-4 w-full">
       <small class="ml-4">Google drive file id</small>
-      @if(!fileId) {
+      @if(!appFolderId()) {
         <div class="h-8 animate-pulse bg-slate-500 rounded col-span-2"></div>
       } @else {
         <div class="w-full h-8 rounded col-span-2 px-2 bg-white flex items-center border border-slate-200 justify-between">
-          <span class="truncate min-w-0">{{fileId}}</span>
-          <a [href]="fileLink" target="_blank" class="ml-1 text-sky-800 text-sm cursor-pointer inline-block flex-shrink-0">View in drive</a>
+          <span class="truncate min-w-0">{{appFolderId()}}</span>
+          <a [href]="appFolderFileLink()" target="_blank" class="ml-1 text-sky-800 text-sm cursor-pointer inline-block flex-shrink-0">View in drive</a>
         </div>
       }
     </label>
@@ -30,17 +30,7 @@ export class GeneralSettingsComponent {
   storageService = inject(StorageService);
 
   storageProvider = computed(() => this.storageService.storageProvider() as GoogleDriveStorageProvider);
+  appFolderId = toSignal(this.storageProvider().appFolderId$);
+  appFolderFileLink = computed(() => `https://drive.google.com/file/d/${this.appFolderId()}/view`);
 
-  fileId?: string;
-  fileLink?: string;
-
-  constructor() {
-    this.storageProvider().appFolderId$.pipe(
-      takeUntilDestroyed()
-    ).subscribe(fileId => {
-      if (!fileId) return;
-      this.fileId = fileId;
-      this.fileLink = `https://drive.google.com/file/d/${fileId}/view`;
-    });
-  }
 }
